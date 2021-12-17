4b5d93e829d8dd1316f0a0d3b070794ae86a63ce
Renato Botini <botinirenato@gmail.com>
2021-12-17 09:20:41 -0300

    Criado Projeto e Implementado o Mapeamento da Estrutura do banco de Dados

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,34 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
+import dominio.Pessoa;
+
+@SuppressWarnings("unused")
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Pessoa p1 = new Pessoa(null,"Renato","botinirenato@gmail.com");
+		Pessoa p2 = new Pessoa(null,"Ricardo","botiniricardo@gmail.com");
+		Pessoa p3 = new Pessoa(null,"Patricia","botinipatricia@gmail.com");
+		Pessoa p4 = new Pessoa(null,"Daniela","botinidaniela@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.persist(p4);
+		em.getTransaction().commit();
+		
+		
+
+	}
+
+}
